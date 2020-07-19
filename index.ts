41351729fde7873549b4f9d5f1371ff3f916880e
Anwesh43 <anweshthecool0@gmail.com>
2020-07-20 05:26:24 +0530

    adding BTLNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class BTLNode {
+
+    prev : BTLNode
+    next : BTLNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BTLNode(this.i)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBTLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BTLNode {
+        var curr : BTLNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
