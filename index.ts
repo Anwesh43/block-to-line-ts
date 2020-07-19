d52a7d64567e8cd1d3ef974aac5102ce8ba29803
Anwesh43 <anweshthecool0@gmail.com>
2020-07-20 04:39:33 +0530

    adding constatnts

---
+++ index.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const parts : number = 3
+const scGap : number = 0.02 / parts
+const strokeFactor : number = 90
+const sizeFactor : number = 5.6
+const colors : Array<String> = ["#2196F3", "#4CAF50", "#f44336", "#FF5722", "#512DA8"]
+const backColor : string = "#BDBDBD"
